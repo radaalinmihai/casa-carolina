04ce158cc9ebe55870644efec9316dc8a65d461e
DontStealMyCodePls <n3gatyv95@gmail.com>
2020-08-22 16:13:55 +0300

    Updates

---
+++ precache-manifest.fda1155e14d0833e86144cf1ac4f3a93.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d890c5385be341f4712281623bccb97",
+    "url": "/vila-carolina/index.html"
+  },
+  {
+    "revision": "df4b76adb287d07728de",
+    "url": "/vila-carolina/static/js/2.39a3b03f.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/vila-carolina/static/js/2.39a3b03f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa094108f1466c3bea97",
+    "url": "/vila-carolina/static/js/main.ce872b16.chunk.js"
+  },
+  {
+    "revision": "7aaec29e733aceb9fece",
+    "url": "/vila-carolina/static/js/runtime-main.d5bbe6bc.js"
+  },
+  {
+    "revision": "425dcb759224052372928b7027b12458",
+    "url": "/vila-carolina/static/media/image_1.425dcb75.jpg"
+  },
+  {
+    "revision": "34a99a5240544bbb51f863ea608fcee7",
+    "url": "/vila-carolina/static/media/image_2.34a99a52.jpg"
+  },
+  {
+    "revision": "928801b8ff79c1464c7e2459a8913471",
+    "url": "/vila-carolina/static/media/image_3.928801b8.jpg"
+  },
+  {
+    "revision": "fadfebf90c486e55735571f244d4ca95",
+    "url": "/vila-carolina/static/media/image_4.fadfebf9.jpeg"
+  },
+  {
+    "revision": "93041331dedf847025665237f01687f2",
+    "url": "/vila-carolina/static/media/mdi_check.93041331.svg"
+  },
+  {
+    "revision": "3ef17fae17f9b3508e4b8fa8e89807fb",
+    "url": "/vila-carolina/static/media/mdi_chevron_left.3ef17fae.svg"
+  },
+  {
+    "revision": "1a644d36b7186c3a30917a05471c6784",
+    "url": "/vila-carolina/static/media/mdi_chevron_right.1a644d36.svg"
+  },
+  {
+    "revision": "5613db24b64483f4b5f68fd55cc50f26",
+    "url": "/vila-carolina/static/media/menu-white-18dp.5613db24.svg"
+  }
+]);
